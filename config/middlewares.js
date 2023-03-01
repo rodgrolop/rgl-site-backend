26684e807a152c824f884aa908d98e62480db99a
Rodrigo Gross López <rodrigogrosslopez@gmail.com>
2023-03-01 14:54:47 +0100

    revert config

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,5 @@
 module.exports = ({ env }) => [
   "strapi::errors",
-  "strapi::security",
   "strapi::cors",
   "strapi::poweredBy",
   "strapi::logger",
@@ -9,4 +8,30 @@ module.exports = ({ env }) => [
   "strapi::session",
   "strapi::favicon",
   "strapi::public",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            `${env("AWS_BUCKET_NAME")}.s3.${env("AWS_REGION")}.amazonaws.com`,
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            `${env("AWS_BUCKET_NAME")}.s3.${env("AWS_REGION")}.amazonaws.com`,
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
 ];
